e5d66d74243d78e69c22b1447feddf8fc6fc9bb1
Melissa <melissapotter.student@careerdevs.com>
2019-01-25 01:13:16 +0000

    html/css

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ 
+ function weather(){
+     var URL = 'https://fcc-weather-api.glitch.me/api/current?lat=53.70&lon=-1.24';
+     $.getJSON(URL, function(){
+         
+     });
+ }   
+    
+});
